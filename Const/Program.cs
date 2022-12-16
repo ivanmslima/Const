d02ec9043f2d628a241783deecce08d3e0ee6cb3
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-12-15 22:13:07 -0300

    Adicionar arquivos de projeto.

---
+++ Const/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            const bool exemplo = true;
+
+            Console.WriteLine("Exemplo de const");
+
+            /*As constantes são valores imutáveis que são conhecidos no tempo de compilação e 
+             * não são alterados durante a vida útil do programa. Constantes são declaradas com o modificador const.
+             * Apenas os tipos internos do C# (excluindo System. Object) podem ser declarados como const*//
+
+
+        }
+    }
+}
